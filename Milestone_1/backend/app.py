ee7dd8a075a81d481eac2ce2f5f611ca26113c18
leanabarbion <leana.barbion@student.ie.edu>
2024-02-29 21:51:25 +0100

    Clean slate

--- Milestone_1/backend/app.py
+++ Milestone_1/backend/app.py
@@ -1,1 +1,33 @@
+from flask import Flask, jsonify
+import requests
+from io import StringIO
+import csv
 
+
+# 93T39LM1F63A0IH9
+app = Flask(__name__)
+YOUR_API_KEY = "demo"
+
+CSV_URL = (
+    f"https://www.alphavantage.co/query?function=LISTING_STATUS&apikey={YOUR_API_KEY}"
+)
+
+
+@app.route("/")
+def list_symbols():
+    with requests.Session() as s:
+        download = s.get(CSV_URL)
+        decoded_content = download.content.decode("utf-8")
+
+        # Use StringIO to convert the string data into a file-like object for csv.reader
+        cr = csv.reader(StringIO(decoded_content), delimiter=",")
+        my_list = list(cr)
+
+        # Extract the "symbol" from each row, assuming 'symbol' is in the first column
+        symbols = [row[0] for row in my_list[1:]]  # Exclude the header row
+
+        return jsonify(symbols)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ Milestone_1/frontend/src/App.js
@@ -0,0 +1,5 @@
+async function logMovies() {
+  const response = await fetch("http://example.com/movies.json");
+  const movies = await response.json();
+  console.log(movies);
+}
